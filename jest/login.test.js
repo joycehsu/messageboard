410370397e1bc7c08efaa0aea20dc413a190d823
joycehsu <joyce.mhhsu@gmail.com>
2021-01-05 14:22:53 +0800

    Update login.test.js

--- jest/login.test.js
+++ jest/login.test.js
@@ -1,78 +1,51 @@
 const request = require('supertest')
 const app = require('../app')
+const clearDB = require('./clearDB')
 
-test("Login Success", async() => {
-    const input = {
-        email : "aabbcc1234@gmail.com",
-        password: "123456abcdef"
+describe('Account Login', () => {
+    let userCookie = ''
+    const correctUser = {
+        email: "login_user@gmail.com",
+        password: "123456"
     }
-    const login = await request(app.callback()).post('/v1/account/login').send(input)
-    expect(login.body.code).toBe(200)
-    expect(login.body.message).toBe("Login success")
-    expect(login.body.data).toBe(true)
-})
 
-test("Login - field[password] format is wrong", async() => {
-    const input = {
-        email : "aabbcc1234",
-        password: "123"
+    const unExistUser = {
+        email: "login_haha@gmail.com",
+        password: "123456"
     }
-    const login = await request(app.callback()).post('/v1/account/login').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Email format is wrong.")
-})
 
-test("Login - field[password] format is wrong", async() => {
-    const input = {
-        email : "aabbcc1234@gmail.com",
-        password: "123"
+    const wrongPasswordUser = {
+        email: "login_user@gmail.com",
+        password: "111111"
     }
-    const login = await request(app.callback()).post('/v1/account/login').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Password format is wrong.")
-})
 
-test("Login - No Fields", async() => {
-    const input = {}
-    const login = await request(app.callback()).post('/v1/account/login').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Need Email and password")
-})
+    beforeAll( async() => {
+        // await clearDB()
+        const userSign = await request(app.callback()).post('/v1/account/signup').send(correctUser)
+    })
 
-test("Login - Loss Field[password]", async() => {
-    const input = {
-        email : "aabbcc1234@gmail.com"
-    }
-    const login = await request(app.callback()).post('/v1/account/login').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Need Email and password")
-})
+    afterAll( async() => {
+        await request(app.callback()).post('/v1/account/logout').set('cookie', userCookie)
+        // await clearDB()
+    })
 
-test("Login - Loss Field[password]", async() => {
-    const input = {
-        password : "1234567"
-    }
-    const login = await request(app.callback()).post('/v1/account/login').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Need Email and password")
-})
-
-test("Login - Email(Account) is not exist", async() => {
-    const input = {
-        email: "Test@gmail.com",
-        password : "1234567"
-    }
-    const login = await request(app.callback()).post('/v1/account/login').send(input)
-    expect(login.body.code).toBe(401)
-    expect(login.body.message).toBe("No register of the email was found.")
-})
-
-test("Login - Password value not correct", async() => {
-    const input = {
-        email: "Test_aabbcc1234@gmail.com",
-        password : "1234567"
-    }
-    const login = await request(app.callback()).post('/v1/account/login').send(input)
-    expect(login.body.code).toBe(403)
-    expect(login.body.message).toBe("The password is not correct.")
+    describe('Go', () => {
+        test("Success", async() => {
+            const login = await request(app.callback()).post('/v1/account/login').send(correctUser)
+            expect(login.body.code).toBe(200)
+            expect(login.body.message).toBe("Login success")
+            expect(login.body.data).toBe(true)
+            userCookie = await login.headers['set-cookie'].join(';')
+        })
+        test("Fail - Email(Account) is not exist", async() => {
+            const login = await request(app.callback()).post('/v1/account/login').send(unExistUser)
+            expect(login.body.code).toBe(401)
+            expect(login.body.message).toBe("No register of the email was found.")
+        })
+        test("Fail - Password value is not correct", async() => {
+            const login = await request(app.callback()).post('/v1/account/login').send(wrongPasswordUser)
+            expect(login.body.code).toBe(403)
+            expect(login.body.message).toBe("The password is not correct.")
+        })
+    })
 })
