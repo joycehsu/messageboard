8c39814de6e0e5726fbb0530b47fc24ab61a34c0
joycehsu <joyce.mhhsu@gmail.com>
2021-01-05 15:59:00 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ let ObjectId = mongoose.Schema.Types.ObjectId
 module.exports = {
     name: "user",
     schema: {
-        _id: { type: ObjectId, default:new mongoose.Types.ObjectId() },
         email: { type:String, required:true, unique:true },
         password: { type:String, required:true },
         createTime: { type:Date, default:Date.now() },
