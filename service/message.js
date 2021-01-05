ea7060ad71ed13a92f7facf7efec81375fd70fb2
joycehsu <joyce.mhhsu@gmail.com>
2021-01-05 14:37:26 +0800

    Update message.js

--- service/message.js
+++ service/message.js
@@ -1,6 +1,11 @@
 const messageBoard = require('../models/index').getModel('message')
 
 const message = {}
+message.findById = async (messageData) => {
+    let result = await messageBoard.findById(messageData)
+    return result
+}
+
 message.check = async (messageData) => {
     let result = await messageBoard.findOne(messageData)
     return result
