254d5bb35fbe235b6e25c2785429133c7253b072
joycehsu <joyce.mhhsu@gmail.com>
2021-01-05 14:23:47 +0800

    Update signup.test.js

--- jest/signup.test.js
+++ jest/signup.test.js
@@ -1,67 +1,74 @@
 const request = require('supertest')
 const app = require('../app')
+const clearDB = require('./clearDB')
 
-test("Signup Success", async() => {
-    const input = {
-        email : "Test_aabbcc1234@gmail.com",
-        password: "123456abcdef"
+describe("Account Signup", () => {
+    const correctUser = {
+        email: "signup_user@gmail.com",
+        password: "123456"
     }
-    const login = await request(app.callback()).post('/v1/account/signup').send(input)
-    expect(login.body.code).toEqual(200)
-    expect(login.body.message).toBe("Create Account success")
-})
 
-test("Signup - field[password] format is wrong", async() => {
-    const input = {
-        email : "aabbcc1234",
+    const wrongEmailUser = {
+        email: "signup_user",
+        password: "123456"
+    }
+
+    const wrongPasswordUser = {
+        email: "signup_user@gmail.com",
         password: "123"
     }
-    const login = await request(app.callback()).post('/v1/account/signup').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Email format is wrong.")
-})
 
-test("Signup - field[password] format is wrong", async() => {
-    const input = {
-        email : "aabbcc1234@gmail.com",
-        password: "123"
+    const lossEmailUser = {
+        password: "123456"
     }
-    const login = await request(app.callback()).post('/v1/account/signup').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Password format is wrong.")
-})
 
-test("Signup - No Fields", async() => {
-    const input = {}
-    const login = await request(app.callback()).post('/v1/account/signup').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Need Email and password")
-})
-
-test("Signup - Loss Field[password]", async() => {
-    const input = {
-        email : "aabbcc1234@gmail.com"
+    const lossPasswordUser = {
+        email: "signup_user@gmail.com"
     }
-    const login = await request(app.callback()).post('/v1/account/signup').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Need Email and password")
-})
 
-test("Signup - Loss Field[password]", async() => {
-    const input = {
-        password : "1234567"
-    }
-    const login = await request(app.callback()).post('/v1/account/signup').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("Need Email and password")
-})
+    beforeAll( async()=>{
+        await clearDB()
+    })
 
-test("Signup - Email(Account) is exist", async() => {
-    const input = {
-        email : "aabbcc1234@gmail.com",
-        password: "1234567"
-    }
-    const login = await request(app.callback()).post('/v1/account/signup').send(input)
-    expect(login.body.code).toBe(400)
-    expect(login.body.message).toBe("The Email is Exist.")
+    afterAll(() => {
+        // await clearDB()
+    })
+
+    describe("Go", () => {
+        test("Success", async() => {
+            const signup = await request(app.callback()).post('/v1/account/signup').send(correctUser)
+            expect(signup.body.code).toBe(200)
+            expect(signup.body.message).toBe("Create Account success")
+        })
+        test("Fail - No field", async() => {
+            const signup = await request(app.callback()).post('/v1/account/signup').send({})
+            expect(signup.body.code).toBe(400)
+            expect(signup.body.message).toBe("Need Email and password")
+        })
+        test("Fail - Loss [email]", async() => {
+            const signup = await request(app.callback()).post('/v1/account/signup').send(lossEmailUser)
+            expect(signup.body.code).toBe(400)
+            expect(signup.body.message).toBe("Need Email and password")
+        })
+        test("Fail - Loss [passowrd]", async() => {
+            const signup = await request(app.callback()).post('/v1/account/signup').send(lossPasswordUser)
+            expect(signup.body.code).toBe(400)
+            expect(signup.body.message).toBe("Need Email and password")
+        })
+        test("Fail - [pmail] format is wrong", async() => {
+            const signup = await request(app.callback()).post('/v1/account/signup').send(wrongEmailUser)
+            expect(signup.body.code).toBe(400)
+            expect(signup.body.message).toBe("Email format is wrong.")
+        })
+        test("Fail - [password] format is wrong", async() => {
+            const signup = await request(app.callback()).post('/v1/account/signup').send(wrongPasswordUser)
+            expect(signup.body.code).toBe(400)
+            expect(signup.body.message).toBe("Password format is wrong.")
+        })
+        test("Fail - Email(Account) is exist", async() => {
+            const signup = await request(app.callback()).post('/v1/account/signup').send(correctUser)
+            expect(signup.body.code).toBe(400)
+            expect(signup.body.message).toBe("The Email is Exist.")
+        })
+    })
 })
